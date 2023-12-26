56f924b3862290e553b2da953f2dbfdb0cf9ddc2
zhulin <zhulinhy@foxmail.com>
2023-12-26 21:44:25 +0800

    zl,feat：修改网站标题，以及预设提示词

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "NextChat",
+  title: "My ChatGpt",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "NextChat",
+    title: "My ChatGpt",
     statusBarStyle: "default",
   },
 };
